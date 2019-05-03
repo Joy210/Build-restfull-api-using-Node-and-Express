4014d7708f0a245d15efc5326a0bc19d8f6af4a8
unknown <md.sohanur.rahman.joy@gmail.com>
2019-05-04 01:56:21 +0600

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+// Array
 const courses = [
     {'id': 1, 'name': 'course_1'},
     {'id': 2, 'name': 'course_2'},
@@ -26,12 +27,13 @@ app.post('/api/courses', (req, res) => {
     const {error} = validateCourse(req.body);
     if(error) return res.status(404).send(error.details[0].message);
 
+    // add item
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
     courses.push(course);
-    res.send(course)
+    res.send(course);
 });
 
 app.put('/api/courses/:id', (req, res) => {
@@ -52,6 +54,7 @@ app.delete('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) return res.status(404).send('The course with the given ID was not found...');
 
+    // delete item
     const index = courses.indexOf(course);
     courses.splice(index, 1);
 
@@ -64,7 +67,7 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
-
+// validation using #Joi
 function validateCourse(course) {
     const schema = {
         name: Joi.string().min(3).required()
